cabf5928a84de1ac2b22969e6baefb5561d39776
Leo Foletto <flttleonardo@gmail.com>
2022-05-18 22:44:20 +0200

    added pydantic custom validators

--- advanced_concepts/01_DTO/05_pydantic.py
+++ advanced_concepts/01_DTO/05_pydantic.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 CURRENT_YEAR = 2022
 
@@ -16,16 +16,25 @@ class MusicAlbum(BaseModel):
     def __init__(__pydantic_self__, **data) -> None:
         super().__init__(**data)
         __pydantic_self__.years_from_publication = CURRENT_YEAR - __pydantic_self__.year
+    
+    @validator('year')
+    @classmethod
+    def check_year_is_valid(cls, value: int) -> None:
+        supported_years = range(1900, CURRENT_YEAR)
+        if not value in supported_years:
+            raise ValueError("Year must be less than or equal to current year and greater than or equal to 1900")
+        return value
 
 if __name__ == "__main__":
     album_params = {
         "title": "The Dark Side of the Moon",
         "artist": "Pink Floyd",
-        "year": 1973,
+        "year": 1873,
         "tracks": ["Speak to Me", "Breathe", "One More Time"]
     }
 
     album = MusicAlbum(title="The Dark Side of the Moon", artist="Pink Floyd", year=1973, tracks=["Speak to Me", "Breathe", "One More Time"])
     album2 = MusicAlbum(title="The Wall", artist="Pink Floyd", year=1979, tracks=["Whatever Happened to My Mother", "The Great Gig in the Sky", "Money"])
     album3 = MusicAlbum(**album_params)
-    print(album == album3)
+    # print(album == album3)
+    print(album3)
--- advanced_concepts/01_DTO/Ex05_Employee_pydantic.py
+++ advanced_concepts/01_DTO/Ex05_Employee_pydantic.py
@@ -1,9 +1,9 @@
 """ Employee -> first name, last name, email, mobile, reports_to, full_name """
 
 from __future__ import annotations
-from typing import Union, Optional
+from typing import Union, Optional, List, Dict, Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Employee(BaseModel):
     first_name: str
@@ -16,22 +16,32 @@ class Employee(BaseModel):
     def __init__(__pydantic_self__, **data) -> None:
         super().__init__(**data)
         __pydantic_self__.full_name = f"{__pydantic_self__.first_name} {__pydantic_self__.last_name}"
+    
+    @validator('email')
+    @classmethod
+    def check_email_is_valid(cls, value: str, values: Dict[str, Any]) -> None:
+        name = values.get('first_name')
+        surname = values.get('last_name')
+        email_address = (name[0] + surname + "@company.com").lower()
+        if value != email_address:
+            raise ValueError(f"Email {value} must be {email_address}")
+        return value
 
 if __name__ == "__main__":
-    boss = Employee(first_name="Boss", last_name="Baby", email="iamtheboss@company.com", mobile="666-666-6666", reports_to="None")
-    employee = Employee(first_name="John", last_name="Doe", email="jdoe1@company.com", mobile="555-555-5555", reports_to=boss)
-    employee2 = Employee(first_name="John", last_name="Doe", email="jdoe1@company.com", mobile="555-555-5555", reports_to="Boss Baby")
+    boss = Employee(first_name="Boss", last_name="Baby", email="bbaby@company.com", mobile="666-666-6666", reports_to="None")
+    employee = Employee(first_name="John", last_name="Doe", email="jdoe@company.com", mobile="555-555-5555", reports_to=boss)
+    employee2 = Employee(first_name="John", last_name="Doe", email="jdoe@company.com", mobile="555-555-5555", reports_to="Boss Baby")
 
     args_dict = {
         "first_name": "John",
         "last_name": "Doe",
-        "email": "john@company.com",
+        "email": "johndoe@company.com",
         "mobile": 12345,
         "reports_to": "Boss Baby"
     }
-    print(employee == employee2)
-    print(employee)
+    # print(employee == employee2)
+    # print(employee)
 
     employee3 = Employee(**args_dict)
     print(employee3)
-    print(employee3.full_name)
+    # print(employee3.full_name)
